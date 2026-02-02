fd62140cb96bd806a142f0854ba5f841ff18f34a
Michael <zhangmichael167@gmail.com>
2026-02-02 19:28:46 +0100

    Adding CSS file processing

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ module.exports = (env) => {
                     test: /\.tsx?$/,
                     use: 'ts-loader',
                     exclude: /node_modules/
+                },
+                {
+                    test: /\.css?$/,
+                    use: ['style-loader', 'css-loader']
                 }
             ]
         },
