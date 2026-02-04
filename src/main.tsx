4c0d035e56fd47a49e018608f961db0a776b40d8
mzhang1 <zhangmichael167@gmail.com>
2026-02-04 14:23:23 +0100

    Update : Renamed main.ts

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import '@/globals.css';
+
+import { createRoot, type Container } from "react-dom/client";
+import MainPage from './pages/MainPage';
+
+const container: Container = (document.body)
+const root = createRoot(container);
