9aadb546208666719a51602e7d6de92c5dc2180a
mzhang1 <zhangmichael167@gmail.com>
2026-02-05 12:15:15 +0100

    Update : Refactored main container

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import '@/globals.css';
 import { createRoot, type Container, type Root } from "react-dom/client";
 import MainPage from './pages/MainPage';
 
-const container: Container = (document.body)
+const container: Container = document.getElementById("mainContent") as Element
 const root: Root = createRoot(container);
 root.render((<MainPage title="Hello world !" />) as React.ReactNode);
