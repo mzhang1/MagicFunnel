800fd406f63a8eeda167d6583659ed68e4b1a0f4
mzhang1 <zhangmichael167@gmail.com>
2026-02-09 19:01:18 +0100

    Update : Adding buttons

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,10 +3,12 @@ import NavbarButton from "@/components/navbar/NavbarButton";
 const Navbar = () => {
     return <div className="w-full h-16 bg-primary flex justify-between">
         <div className="h-full flex">
-
+            <NavbarButton title="Button 1" />
+            <NavbarButton title="Button 2" />
         </div>
         <div className="h-full flex">
-
+            <NavbarButton title="Button right 1" />
+            <NavbarButton title="Button right 2" />
         </div>
     </div>;
 }
