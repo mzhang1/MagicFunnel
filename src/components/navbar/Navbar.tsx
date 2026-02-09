d4be04e4666edd53c15bf540cb78bf02bc18e8a3
mzhang1 <zhangmichael167@gmail.com>
2026-02-09 19:49:25 +0100

    Update : Slightly reduced navbar size

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import NavbarButton from "@/components/navbar/NavbarButton";
 const Navbar = () => {
     const blockClassList = "h-full flex gap-1 p-0";
 
-    return <nav className="w-full h-16 bg-primary flex justify-between text-primary-font">
+    return <nav className="w-full h-14 bg-primary flex justify-between text-primary-font">
         <ul className={blockClassList}>
             <NavbarButton title="Button 1" />
             <NavbarButton title="Button 2" />
