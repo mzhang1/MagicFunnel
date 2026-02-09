b438947928d47e73d65d3d89fb5bf4f106836fc5
mzhang1 <zhangmichael167@gmail.com>
2026-02-09 19:28:00 +0100

    Update : Applying new font color

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import NavbarButton from "@/components/navbar/NavbarButton";
 const Navbar = () => {
     const blockClassList = "h-full flex gap-1 p-8";
 
-    return <div className="w-full h-16 bg-primary flex justify-between">
+    return <div className="w-full h-16 bg-primary flex justify-between text-primary-font">
         <div className={blockClassList}>
             <NavbarButton title="Button 1" />
             <NavbarButton title="Button 2" />
