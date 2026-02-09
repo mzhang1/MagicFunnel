2cbb6cb436c54022121434886a57134d643a7f89
mzhang1 <zhangmichael167@gmail.com>
2026-02-09 19:45:49 +0100

    Update : changed html tags

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,16 +3,16 @@ import NavbarButton from "@/components/navbar/NavbarButton";
 const Navbar = () => {
     const blockClassList = "h-full flex gap-1 p-0";
 
-    return <div className="w-full h-16 bg-primary flex justify-between text-primary-font">
-        <div className={blockClassList}>
+    return <nav className="w-full h-16 bg-primary flex justify-between text-primary-font">
+        <ul className={blockClassList}>
             <NavbarButton title="Button 1" />
             <NavbarButton title="Button 2" />
-        </div>
-        <div className={blockClassList}>
+        </ul>
+        <ul className={blockClassList}>
             <NavbarButton title="Button right 1" />
             <NavbarButton title="Button right 2" />
-        </div>
-    </div>;
+        </ul>
+    </nav>;
 }
 
 export default Navbar;
