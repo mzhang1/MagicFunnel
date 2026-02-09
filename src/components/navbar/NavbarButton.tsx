87ba8da772907bdff24856a11b710473cfd90593
mzhang1 <zhangmichael167@gmail.com>
2026-02-09 19:38:14 +0100

    Update : Adding button padding

--- src/components/navbar/NavbarButton.tsx
+++ src/components/navbar/NavbarButton.tsx
@@ -8,7 +8,7 @@ type NavbarButtonOptions = {
 const NavbarButton = ( { title, disabled, onClick, onHover } : NavbarButtonOptions ) => {
     const buttonStatus: string = !disabled ? "cursor-pointer" : "";
 
-    return <button className={"h-full " + buttonStatus} onClick={onClick} onMouseOver={onHover}>
+    return <button className={"h-full p-3 " + buttonStatus} onClick={onClick} onMouseOver={onHover}>
         { title }
     </button>
 };
