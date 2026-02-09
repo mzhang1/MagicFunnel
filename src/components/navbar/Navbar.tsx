d209703c38c4222a25755f1106948becda2f4412
mzhang1 <zhangmichael167@gmail.com>
2026-02-09 13:01:21 +0100

    Update : Adding classes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,10 @@
 const Navbar = () => {
     return <div className="w-full h-16 bg-primary flex justify-around">
-        <div>
+        <div className="h-full flex">
 
         </div>
-        <div>
-            
+        <div className="h-full flex">
+
         </div>
     </div>;
 }
