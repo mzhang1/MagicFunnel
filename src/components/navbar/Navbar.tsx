ff2b2b5ccb1142d1a39decd4b4a603ecc5143797
mzhang1 <zhangmichael167@gmail.com>
2026-02-09 12:00:28 +0100

    Update : Setting up navbar divs

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,11 @@
 const Navbar = () => {
     return <div className="w-full h-16 bg-primary flex justify-around">
-        
+        <div>
+
+        </div>
+        <div>
+            
+        </div>
     </div>;
 }
 
