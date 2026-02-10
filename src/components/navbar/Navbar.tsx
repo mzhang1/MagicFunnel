1b8cdf36188792b45eabec6ac640c6e32412ce48
mzhang1 <zhangmichael167@gmail.com>
2026-02-10 21:49:12 +0100

    Setting up destructor function

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import NavbarButton from "@/components/navbar/NavbarButton";
 import NavbarDropdown from "@/components/navbar/NavbarDropdown";
 
 const Navbar = () => {
-    const [currentHoveredButton, updateHoveredButton] = useState("");
+    let [currentHoveredButton, updateHoveredButton] = useState("");
+
+    useEffect(() => {
+        return () => {
+            currentHoveredButton = "";
+        }
+    }, [currentHoveredButton])
 
     const blockClassList = "h-full flex gap-1 p-0";
 
     return <nav className="w-full h-14 bg-primary flex justify-between text-primary-font">
         <ul className={blockClassList}>
-            <NavbarButton title="Button 1" />
+            <NavbarButton title="Button 1"  />
             <NavbarButton title="Button 2" />
         </ul>
         <ul className={blockClassList}>
