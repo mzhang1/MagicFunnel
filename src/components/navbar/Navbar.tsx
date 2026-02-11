43379fdf62a828fee6e6d32b5fc059d30d3de21d
mzhang1 <zhangmichael167@gmail.com>
2026-02-11 12:16:46 +0100

    Testing : Single block nav menu

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -16,14 +16,12 @@ const Navbar = () => {
 
     const blockClassList = "h-full flex gap-1 p-0";
 
-    return <nav className="w-full h-14 bg-primary flex justify-between text-primary-font">
+    return <nav className="w-full h-14 bg-primary flex justify-center text-primary-font">
         <ul className={blockClassList}>
             <NavbarButton title="Button 1" onHover={() => updateHoveredButton("Button 1")} />
             <NavbarButton title="Button 2" onHover={() => updateHoveredButton("Button 2")} />
-        </ul>
-        <ul className={blockClassList}>
-            <NavbarButton title="Button right 1" onHover={() => updateHoveredButton("Button right 1")} />
-            <NavbarButton title="Button right 2" onHover={() => updateHoveredButton("Button right 2")} />
+            <NavbarButton title="Button 3" onHover={() => updateHoveredButton("Button 3")} />
+            <NavbarButton title="Button 4" onHover={() => updateHoveredButton("Button 4")} />
         </ul>
     </nav>;
 }
