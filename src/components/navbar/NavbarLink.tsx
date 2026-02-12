09b56b39fe3ed65a131f3877dcc6e8b287b732ac
Michael Zhang <zhangmichael167@gmail.com>
2026-02-12 10:53:20 +0100

    Update : Creating type for NavbarLinkData

--- src/components/navbar/NavbarLink.tsx
+++ src/components/navbar/NavbarLink.tsx
@@ -1,5 +1,10 @@
-const NavbarLink = () => {
+type NavbarLinkData = {
+    text: string,
+    href: string
+}
 
+const NavbarLink = ({ text, href } : NavbarLinkData) => {
+    return <a href={href} className="text-dark-blue-link">{text}</a>
 }
 
 export default NavbarLink;
