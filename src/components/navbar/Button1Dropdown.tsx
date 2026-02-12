8d7f707bcb9d28eaf67422f758302208892aeb18
Michael Zhang <zhangmichael167@gmail.com>
2026-02-12 10:40:14 +0100

    Update : Setting background color of dropdown

--- src/components/navbar/Button1Dropdown.tsx
+++ src/components/navbar/Button1Dropdown.tsx
@@ -1,5 +1,5 @@
 export const Button1Dropdown = () => {
-    return <div className="w-sm">
+    return <div className="w-sm bg-white">
         <a className="text-dark-blue-link">Link 1</a>
         <a className="text-dark-blue-link">Link 2</a>
         <a className="text-dark-blue-link">Link 3</a>
