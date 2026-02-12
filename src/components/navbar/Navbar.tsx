60f164cfb8c9276e4831368b88ebbe44275de2b3
mzhang1 <zhangmichael167@gmail.com>
2026-02-12 12:08:51 +0100

    Shortening lines

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -50,7 +50,10 @@ const Navbar = () => {
             {
                 buttonList.map((singleButton: NavbarButtonData) => {
                     return <li>
-                        <NavbarButton title={singleButton.text} onHover={() => updateHoveredButton(singleButton.id)}></NavbarButton>
+                        <NavbarButton 
+                            title={singleButton.text} 
+                            onHover={() => updateHoveredButton(singleButton.id)}>
+                        </NavbarButton>
                         {
                             currentHoveredButton == singleButton.id ?
                             <NavbarDropdown>
