0cd71402353deaf1b73cddf0845c1b0e411f0835
mzhang1 <zhangmichael167@gmail.com>
2026-02-12 11:50:37 +0100

    Update : Importing new NavbarLinkData type

--- src/components/navbar/Button1Dropdown.tsx
+++ src/components/navbar/Button1Dropdown.tsx
@@ -1,4 +1,21 @@
+import type { NavbarLinkData } from '@/components/navbar/NavbarLink';
+
 export const Button1Dropdown = () => {
+    const links: NavbarLinkData[] = [
+        {
+            text: "Link1",
+            href: ""
+        },
+        {
+            text: "Link2",
+            href: "",
+        },
+        {
+            text: "Link3",
+            href: ""
+        }
+    ];
+
     return <div className="w-sm bg-white">
         <a className="text-dark-blue-link">Link 1</a>
         <a className="text-dark-blue-link">Link 2</a>
