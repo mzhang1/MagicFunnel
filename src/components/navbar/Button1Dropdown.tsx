95cbea4354a9138f52f6fe405394f6e909678ff4
Michael Zhang <zhangmichael167@gmail.com>
2026-02-12 10:36:00 +0100

    Update : Populating one dropdown

--- src/components/navbar/Button1Dropdown.tsx
+++ src/components/navbar/Button1Dropdown.tsx
@@ -1,5 +1,7 @@
 export const Button1Dropdown = () => {
-    return <div>
-
+    return <div className="w-sm">
+        <a className="text-dark-blue-link">Link 1</a>
+        <a className="text-dark-blue-link">Link 2</a>
+        <a className="text-dark-blue-link">Link 3</a>
     </div>
 }
