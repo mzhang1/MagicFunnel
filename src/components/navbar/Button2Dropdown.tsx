3ddd13f1f06a349df31cdba5c587c0ca9265a19d
Michael <zhangmichael167@gmail.com>
2026-02-13 13:45:58 +0100

    Update : Added test links

--- src/components/navbar/Button2Dropdown.tsx
+++ src/components/navbar/Button2Dropdown.tsx
@@ -1,5 +1,24 @@
-export const Button2Dropdown = () => {
-    return <div>
+import type { NavbarLinkData } from '@/components/navbar/NavbarLink';
 
+export const Button2Dropdown = () => {
+    const links: NavbarLinkData[] = [
+        {
+            text: "Link1",
+            href: ""
+        },
+        {
+            text: "Link2",
+            href: "",
+        },
+        {
+            text: "Link3",
+            href: ""
+        }
+    ];
+
+    return <div className='w-sm'>
+        {links.map((singleLink: NavbarLinkData) => {
+            return <a href={singleLink.href}>{singleLink.text}</a>
+        })}
     </div>
 }
