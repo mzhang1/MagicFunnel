9c77eb9b0c6fe834dd725f0c112b28b42a284e03
Michael <zhangmichael167@gmail.com>
2026-02-14 02:01:04 +0100

    Style update : Changing cursor to pointer

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -41,7 +41,7 @@ const Navbar = () => {
             {
                 buttonList.map((singleButton: NavbarButtonData, index: React.Key) => {
                     return <li key={index} 
-                        className="w-40 flex justify-center items-center" 
+                        className="w-40 flex justify-center items-center cursor-pointer" 
                         onMouseOver={() => updateHoveredButton(singleButton.id)}
                     >
                         <>{singleButton.text}</>
