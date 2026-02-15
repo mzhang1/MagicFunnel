5522ea061195c2bbd257fdf2e569730b461a4fcb
Michael <zhangmichael167@gmail.com>
2026-02-15 11:53:39 +0100

    Update : Importing section contents

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,8 @@
 import Navbar from "@/components/navbar/Navbar";
 
+import Section1Content from "@/components/mainpagesection/Section1Content";
+import Section2Content from "@/components/mainpagesection/Section2Content";
+
 const MainPage = () => {
     return <div className="overflow-x-hidden overflow-y-scroll">
         <Navbar />
