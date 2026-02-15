b2d8b9fc9abfaf48f00ee642d2986047628cfaae
Michael <zhangmichael167@gmail.com>
2026-02-15 11:51:09 +0100

    Removed : Generic main page section

--- src/components/mainpagesection/MainPageSection.tsx
+++
@@ -1,7 +0,0 @@
-const MainPageSection = ({ additionalClasses, children } : { additionalClasses?: string, children: React.ReactNode }) => {
-    return <section className="w-full h-1/2">
-        {children}
-    </section>
-}
-
-export default MainPageSection;
