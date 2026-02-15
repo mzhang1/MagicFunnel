fd9ef6ca923f48adb2f4855348aa0cea84be3079
Michael <zhangmichael167@gmail.com>
2026-02-15 12:03:14 +0100

    Update : Adding SectionData definition

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,8 +5,24 @@ import Section2Content from "@/components/mainpagesection/Section2Content";
 import type { SectionData } from "@/components/mainpagesection/types";
 
 const MainPage = () => {
+    const sectionList: SectionData[] = [
+        {
+            id: "section1",
+            component: <Section1Content />
+        },
+        {
+            id: "section2",
+            component: <Section2Content />
+        }
+    ];
+
     return <div className="overflow-x-hidden overflow-y-scroll">
         <Navbar />
+        {
+            sectionList.map((singleSection: SectionData, index: React.Key) => {
+                return singleSection.component;
+            });
+        }
     </div>
 } 
 
