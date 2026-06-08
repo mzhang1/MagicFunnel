dd5d23174a85eaaf2e12c6357bc4b5bae67d97fb
Michael <zhangmichael167@gmail.com>
2026-06-08 11:45:07 +0200

    Update : Creating a dropdown subdirectory

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 import NavbarButton from "@/components/navbar/NavbarButton";
 import NavbarDropdown from "@/components/navbar/NavbarDropdown";
-import { Button1Dropdown } from "@/components/navbar/Button1Dropdown";
+import { Button1Dropdown } from "@/components/navbar/dropdowns/Button1Dropdown";
 import { Button2Dropdown } from "@/components/navbar/Button2Dropdown";
 import { Button3Dropdown } from "@/components/navbar/Button3Dropdown";
 import { Button4Dropdown } from "@/components/navbar/Button4Dropdown";
