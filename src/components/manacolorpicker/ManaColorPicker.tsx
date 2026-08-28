8fce4ff2a373d8381fd433f00a4ccb1bf044ee5f
Michael <zhangmichael167@gmail.com>
2026-08-28 15:21:52 +0200

    Update : Creating mana symbol image files imports

--- src/components/manacolorpicker/ManaColorPicker.tsx
+++ src/components/manacolorpicker/ManaColorPicker.tsx
@@ -1,3 +1,9 @@
+import WhiteManaSymbol from '@images/mana/w.svg';
+import BlueManaSymbol from '@images/mana/u.svg';
+import BlackManaSymbol from '@images/mana/b.svg';
+import RedManaSymbol from '@images/mana/r.svg';
+import GreenManaSymbol from '@images/mana/g.svg';
+
 export default function ManaColorPicker(){
     
 }
