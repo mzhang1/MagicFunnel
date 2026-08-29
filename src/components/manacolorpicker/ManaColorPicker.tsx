827d6bc546be7a51a8e990ce57d6aecd63cfe771
Michael <zhangmichael167@gmail.com>
2026-08-29 10:06:34 +0200

    Update : Importing hook and type from useManaColors

--- src/components/manacolorpicker/ManaColorPicker.tsx
+++ src/components/manacolorpicker/ManaColorPicker.tsx
@@ -1,9 +1,16 @@
+import { useEffect } from 'react';
+import type { ManaColorToggle } from '@/hooks/useManaColors';
+import useManaColors from '@/hooks/useManaColors';
+
 import WhiteManaSymbol from '@images/mana/w.svg';
 import BlueManaSymbol from '@images/mana/u.svg';
 import BlackManaSymbol from '@images/mana/b.svg';
 import RedManaSymbol from '@images/mana/r.svg';
 import GreenManaSymbol from '@images/mana/g.svg';
 
-export default function ManaColorPicker(){
-    
+
+export default function ManaColorPicker(defaultManaColorConfig: ManaColorToggle){
+    useEffect(() => {
+
+    },[defaultManaColorConfig]);
 }
