9aeed4384de8ca5aee11ff30475b764f36a39c34
Michael <zhangmichael167@gmail.com>
2026-08-29 11:44:09 +0200

    Temporary update : Adding ManaColorPicker component

--- src/components/mainpagesection/Section1Content.tsx
+++ src/components/mainpagesection/Section1Content.tsx
@@ -1,6 +1,19 @@
+import ManaColorPicker from "@/components/manacolorpicker/ManaColorPicker";
+import type { ManaColorToggle } from '@/hooks/useManaColors';
+
 const Section1Content = () => {
+    const defaultManaColorPicked: ManaColorToggle = {
+        White: true,
+        Blue: true,
+        Black: true,
+        Red: true,
+        Green: true,
+        Colorless: true
+    };
+
     return <section key="section1" className="w-full h-120">
-        {"Section 1"}
+        <ManaColorPicker defaultManaColorConfig={defaultManaColorPicked}>
+        </ManaColorPicker>
     </section>
 }
 
