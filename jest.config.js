a980684d79d925ef1dbf766a61d1b38be5c307f0
mzhang1 <zhangmichael167@gmail.com>
2026-09-02 14:26:55 +0200

    Update : Jest is now using tsconfig.test.ts paths aliases

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 /** @type {import("jest").Config} **/
+
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./test/tsconfig.test.json");
+
 module.exports = {
-  testEnvironment: "node",
+  preset: "ts-jest",
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths), 
   transform: {
     '^.+\\.(ts|tsx)$': ['ts-jest', {
       tsconfig: './test/tsconfig.test.json'
