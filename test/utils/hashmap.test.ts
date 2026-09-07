0cfcb1246338b46ec43901e3d514173f343fb479
Michael <zhangmichael167@gmail.com>
2026-09-07 16:30:16 +0200

    Update : Creating default pattern for test

--- test/utils/hashmap.test.ts
+++ test/utils/hashmap.test.ts
@@ -1,3 +1,7 @@
-describe('Hashtable check functions', () => {
+import { HashMap } from "./../../src/utils/hashmap";
 
+describe('Hashtable check functions', () => {
+    test("empty test", () => {
+        const hashmap: HashMap<number, string> = new HashMap<number, string>();
+    })
 });
