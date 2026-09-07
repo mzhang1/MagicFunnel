507f1d524d0ff491c609be4afd7761f90c7b780b
mzhang1 <zhangmichael167@gmail.com>
2026-09-07 20:55:49 +0200

    Update : moved jest.config.js into the test directory

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import("jest").Config} **/
 
 const { pathsToModuleNameMapper } = require("ts-jest");
-const { compilerOptions } = require("./test/tsconfig.test.json");
+const { compilerOptions } = require("./tsconfig.test.json");
 
 module.exports = {
   preset: "ts-jest",
