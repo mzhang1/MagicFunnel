7230d7a418ec7cad340e5435b99fce9df97cfba9
mzhang1 <zhangmichael167@gmail.com>
2026-09-09 19:13:48 +0200

    Update : Removed ts-jest usage, replacing them with babel-jest

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,16 +1,7 @@
 /** @type {import("jest").Config} **/
 
-import { pathsToModuleNameMapper } from "ts-jest";
-import { compilerOptions } from "./tsconfig.test.json";
-
 export default {
-  preset: "ts-jest",
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths), 
   transform: {
-    '^.+\\.(ts|tsx)$': [
-      'ts-jest', {
-        tsconfig: './test/tsconfig.test.json'
-      }
-    ]
+    '^.+\\.(ts|tsx)$': 'babel-jest'
   }
 };
