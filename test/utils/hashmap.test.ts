d2a24a94d664277bbf53ae79a8951be795bbe147
mzhang1 <zhangmichael167@gmail.com>
2026-09-10 11:34:38 +0200

    Update : Adding assertion

--- test/utils/hashmap.test.ts
+++ test/utils/hashmap.test.ts
@@ -10,5 +10,6 @@ describe('Hashtable basic functions', () => {
         hashmap.set(1, "The Queen of Dale");
         hashmap.set(2, "The Royal Scions");
         hashmap.set(3, "Force of Will");
+        expect(hashmap.count()).toBe(3);
     });
 });
