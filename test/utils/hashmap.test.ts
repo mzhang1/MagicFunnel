7cc21488de208276aa3150d2aeabde78e00af9ce
mzhang1 <zhangmichael167@gmail.com>
2026-09-10 10:59:44 +0200

    Update : Created first unit test (null check)

--- test/utils/hashmap.test.ts
+++ test/utils/hashmap.test.ts
@@ -1,7 +1,8 @@
 import { HashMap } from "./../../src/utils/hashmap";
 
 describe('Hashtable check functions', () => {
-    test("empty test", () => {
+    it("should not be null", () => {
         const hashmap: HashMap<number, string> = new HashMap<number, string>();
+        expect(hashmap).not.toBeNull();
     })
 });
