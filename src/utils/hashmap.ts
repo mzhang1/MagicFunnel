3f51cc7621bef49953122c6ac8a92da6dbf01fb6
mzhang1 <zhangmichael167@gmail.com>
2026-09-10 13:48:52 +0200

    Update : Implementing base of map function

--- src/utils/hashmap.ts
+++ src/utils/hashmap.ts
@@ -18,6 +18,9 @@ export class HashMap<K, V>
 
     map<R>(inputFunc: <K, V, R>(value: V, key: K) => R) : R[] {
         const result: R[] = [];
+        this.dataSet.forEach((value: V, key: K) => {
+            result.push(inputFunc<K, V, R>(value, key));
+        })
         return result;
     };
 }
