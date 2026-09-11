2ccd7b878c6287c2262de9de16cf75d9a6599122
Michael <zhangmichael167@gmail.com>
2026-09-11 12:42:30 +0200

    Implemented : Hashmap.map

--- src/utils/hashmap.ts
+++ src/utils/hashmap.ts
@@ -28,6 +28,10 @@ export class HashMap<K extends number | string | symbol, V>
      */
     map<R>(inputFunc: CallbackFunc<K, V, R>) : Partial<Record<K, R>> {
         let result: Partial<Record<K,R>> = {};
+        this.dataSet.forEach((value: V, key: K) => {
+            const recordResult: R = inputFunc(key, value);
+            result[key] = recordResult;
+        });
         return result;
     };
 }
