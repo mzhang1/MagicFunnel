ccbf8aeef25d302cb5d5668b99e70b053ba7cf67
Michael <zhangmichael167@gmail.com>
2026-09-11 12:29:54 +0200

    Documenting hashmap.map

--- src/utils/hashmap.ts
+++ src/utils/hashmap.ts
@@ -18,6 +18,14 @@ export class HashMap<K extends number | string | symbol, V>
         return this.dataSet.size;
     }
 
+    /**
+     * Loops through all the records of this.dataSet and performs the inputFunc for each
+     * of them, expecting to return a value for each. this aims to emulate array.map from 
+     * standard JS
+     * 
+     * @param inputFunc Callback function for mapping
+     * @returns A Record variable with the specified generic type. Can be empty with Partial Utility type
+     */
     map<R>(inputFunc: CallbackFunc<K, V, R>) : Partial<Record<K, R>> {
         let result: Partial<Record<K,R>> = {};
         return result;
