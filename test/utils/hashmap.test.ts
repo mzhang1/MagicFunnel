ccb8180897b69b0f8e18ed52d13656c51f473615
Michael <zhangmichael167@gmail.com>
2026-09-11 11:25:57 +0200

    Update : Instanciating Hashmap doesn't need placeholder type declaration

--- test/utils/hashmap.test.ts
+++ test/utils/hashmap.test.ts
@@ -1,7 +1,7 @@
 import { HashMap } from "@/utils/hashmap";
 
 describe('Hashtable basic functions', () => {
-    let hashmap: HashMap<number, string> = new HashMap<number, string>();
+    let hashmap: HashMap<number, string> = new HashMap();
     it("should not be null", () => {
         expect(hashmap).not.toBeNull();
     });
