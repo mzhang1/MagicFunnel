0b62c21c3a02d9fa9076fe93ee1a60ea098d458d
mzhang1 <zhangmichael167@gmail.com>
2026-09-23 14:27:05 +0200

    Removed : Mentions of ManaColorPicker type

--- src/components/mainpagesection/Section1Content.tsx
+++ src/components/mainpagesection/Section1Content.tsx
@@ -1,18 +1,8 @@
 import ManaColorPicker from "@/components/manacolorpicker/ManaColorPicker";
-import type { ManaColorToggle } from '@/hooks/useManaColors';
 
 const Section1Content = () => {
-    const defaultManaColorPicked: ManaColorToggle = {
-        White: true,
-        Blue: true,
-        Black: true,
-        Red: true,
-        Green: true,
-        Colorless: true
-    };
-
     return <section key="section1" className="w-full h-120">
-        <ManaColorPicker defaultManaColorConfig={defaultManaColorPicked}>
+        <ManaColorPicker>
         </ManaColorPicker>
     </section>
 }
--- src/hooks/useManaColors.tsx
+++ src/hooks/useManaColors.tsx
@@ -2,15 +2,6 @@ import { useState } from "react";
 import { Color } from "@/enums";
 import { HashMap } from "@/utils/hashmap";
 
-export type ManaColorToggle = {
-    White: boolean,
-    Blue: boolean,
-    Black: boolean,
-    Red: boolean,
-    Green: boolean,
-    Colorless: boolean
-}
-
 export default function useManaColors(colorToggle: HashMap<Color, boolean>) :
 [
     HashMap<Color, boolean>,
